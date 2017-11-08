d90460548cd6cfc8c43d2842b7a8c32c3117970e
Alex <aslbarnett@gmail.com>
2017-11-08 06:35:48 +0000

    made error page

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const Twit = require('twit');
+const ta = require('time-ago')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const API_KEYS = require('./config');
 
@@ -8,6 +12,9 @@ const T = new Twit(API_KEYS);
 const screenName = 'Alex_Barnett';
 const userId = '28107277';
 const numberOfInfo = 5;
+let dataObj = {};
+let messageDates = [];
+let data;
 
 function getTweetData() {
     return T.get(`statuses/user_timeline.json?screen_name=${screenName}&count=${numberOfInfo}`)
@@ -48,7 +55,23 @@ function getDirectMessages() {
             return result.data;
         });
 }
+let date = new Date('Thu Aug 23 19:45:07 +0000 2012');
+console.log(date.getFullYear());
 
+function formatDate(date) {
+    const properDate = new Date(date);
+    const currentDate = new Date();
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    if (properDate.getFullYear() < currentDate.getFullYear()) {
+        let days = properDate.getDate();
+        let month = months[properDate.getMonth()].slice(0, 3);
+        let year = properDate.getFullYear();
+        return `${days} ${month} ${year}`;
+    } else if (properDate.getFullYear() === currentDate.getFullYear()) {
+        return ta.ago(properDate);
+    }
+}
 
 
 //const tweets = getTwitterData();
@@ -56,20 +79,79 @@ function getDirectMessages() {
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
-app.get('/', async function(req, res) {
-    let dataObj = {};
-    const data = await Promise.all([getProfileData(), getTweetData(), getFollowingData(), getDirectMessages()]);
-    dataObj.profile = data[0];
-    dataObj.tweets = data[1];
-    dataObj.followers = data[2];
-    dataObj.messages = data[3];
-    console.log(dataObj.messages[1].text);
-    res.render('index', {dataObj});
+app.get('/', async function(req, res, next) {
+    data = await Promise.all([getProfileData(), getTweetData(), getFollowingData(), getDirectMessages()]);
+
+    if (data !== undefined && data.length === 4) {
+        dataObj.profile = data[0];
+        let tweets = data[1];
+        dataObj.followers = data[2];
+        dataObj.messages = data[3];
+
+        tweets.forEach(tweet => {
+            tweet.timelineDate = formatDate(tweet.created_at);
+        });
+
+        dataObj.tweets = tweets;
+
+        // message date
+        dataObj.messages.forEach(message => {
+            messageDates.push(formatDate(message.created_at));
+        });
+
+        dataObj.messageDates = messageDates;
+    } else {
+        next();
+    }
+
+    res.render('index',{dataObj});
+});
+
+app.post('/', (req, res) => {
+    const tweet = req.body.tweetEntry;
+    T.post('statuses/update', { status: tweet }, async function(err, data, response) {
+        data = await Promise.all([getProfileData(), getTweetData(), getFollowingData(), getDirectMessages()]);
+
+        if (data !== undefined && data.length === 4) {
+            dataObj.profile = data[0];
+            let tweets = data[1];
+            dataObj.followers = data[2];
+            dataObj.messages = data[3];
+
+            tweets.forEach(tweet => {
+                tweet.timelineDate = formatDate(tweet.created_at);
+            });
+
+            dataObj.tweets = tweets;
+
+            // message date
+            dataObj.messages.forEach(message => {
+                messageDates.push(formatDate(message.created_at));
+            });
+
+            dataObj.messageDates = messageDates;
+        } else {
+            next();
+        }
+        res.send(dataObj);
+    });
 });
 
 // Twitter -- GET statuses/user_timeline for latest 5 tweets
 // Twitter -- GET friends/ids for latest 5 friends
 // Twitter -- GET direct_messages/events/list for latest 5 messages
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error', err);
+});
 
 app.listen(3000, () => {
     console.log('The application is running on port 3000!');
